961cf196bf29927650c7b5d471c9a11a3644f1ec
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2024-12-23 09:46:04 +0500

    Configured next js and also added the app directory for routing and also added the components folder

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",  // App directory (if using App Router)
+    "./pages/**/*.{js,ts,jsx,tsx}", // Pages directory (if using Pages Router)
+    "./components/**/*.{js,ts,jsx,tsx}", // Components directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
