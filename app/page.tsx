9b23a76e822049244fe41da0f8506b82a36edff1
Rabie <u7229988@anu.edu.au>
2024-12-27 15:38:20 +0500

    Remove commented-out code and clean up Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,9 @@
-// import React from 'react'
-// import MenuItem  from '../components/MenuItem'
-
-// const page = () => {
-//   return (
-//     <MenuItem/>
-
-//   );
-// }
-
-// export default page
-
-import MenuList from "../components/MenuList";
-
 import HeroSection from "../components/HomeBody";
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
-    
-
-     
+      <HeroSection />     
     </>
   );
 }
