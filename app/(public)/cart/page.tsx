be64e87dd0116ea8521fb532b3ac4e815123ceb2
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-12-02 11:04:28 +0000

    fixed cart build error

--- app/(public)/cart/page.tsx
+++ app/(public)/cart/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ShoppingCart from '@components/ShoppingCart/ShoppingCart'
 
+export const dynamic = "force-dynamic";
 const page = () => {
   return (
     <ShoppingCart />
