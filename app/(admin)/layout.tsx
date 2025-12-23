97ef2d84befea072e242c4d5f2d9fd4e1273eb47
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-12-23 11:42:53 +0500

    added favicon to the website and also removed the unncessary images

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -3,6 +3,18 @@ import { Toaster } from "react-hot-toast";
 import { MenuProvider } from "@utils/context/MenuContext";
 import SideBar from "@components/Admin/SideBar";
 import { OrdersProvider } from "@utils/context/OrderContext";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kebabscrib Admin",
+  icons: {
+    icon: {
+      url: "/assets/KC_Logo_Logomark_green.svg",
+      type: "image/svg+xml",
+      sizes: "any",
+    },
+  },
+};
 
 export default function AdminLayout({
   children,
